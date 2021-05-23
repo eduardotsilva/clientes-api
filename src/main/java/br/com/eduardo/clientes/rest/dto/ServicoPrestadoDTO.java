134d3e51228af3c5b6ff828a5bda402afad0989f
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-23 12:31:20 -0300

    84. Mensagemde feedback no formulário de serviços prestados

--- src/main/java/br/com/eduardo/clientes/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/br/com/eduardo/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServicoPrestadoDTO {
 
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String Descricao;
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 
 

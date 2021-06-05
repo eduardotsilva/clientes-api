459ea3a63090d55fe2f3d2ad8f7ef9105b8e8a7c
NoteEdu <eduardo.tsilva@hotmail.com>
2021-06-05 12:44:05 -0300

    correção application properties

--- src/main/java/br/com/eduardo/clientes/config/ResourceServerConfig.java
+++ src/main/java/br/com/eduardo/clientes/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package br.com.eduardo.clientes.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -14,9 +15,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/usuarios","/h2-console/**", "/swagger-ui.html","/webjars/**","/swagger-ui.html/**", "/swagger-resources/**","/v2/api-docs").permitAll()
-                .antMatchers("/api/clientes/**","/api/servicos-prestados/**").authenticated()
-                .anyRequest().denyAll();
+                .antMatchers("**").permitAll();
+//                .antMatchers("/api/usuarios","/h2-console/**", "/swagger-ui.html","/webjars/**","/swagger-ui.html/**", "/swagger-resources/**","/v2/api-docs").permitAll()
+//                .antMatchers("/api/clientes/**","/api/servicos-prestados/**").authenticated()
+//                .anyRequest().denyAll();
         ;
     }
 }
